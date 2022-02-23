15a8139635901f140e8c3dd6663c11158b926a82
Debayan <sdebayan999@gmail.com>
2022-02-23 23:00:00 +0530

    login and logout implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,23 @@ import Navbar from './components/Navbar';
 import Glassform from './components/Glassform';
 import Register from './components/Register';
 import Chat from './pages/Chat';
+import PrivateComponent from './components/PrivateComponent';
 
 function App() {
   return (
     <>
     <BrowserRouter>
-    {/* <Navbar val="Login/Signup"/> */}
+    <Navbar val="Login/Signup"/>
     <Routes>
 
-      <Route path="/chat" element={<><Navbar/><Chat/></>} />
+      <Route element={<PrivateComponent/>}>
+        <Route path="/chat" element={<><Chat/></>} />
+      </Route>
 
-      <Route path="/" element={<><Navbar val="Login/Signup" /><h1>This is Home</h1></>} />
-      <Route path="/about" element={<><Navbar val="Login/Signup" /><h1>This is About us</h1></>} />
-      <Route path="/contact" element={<><Navbar val="Login/Signup" /><h1>This is contact</h1></>} />
-      <Route path="/download" element={<><Navbar val="Login/Signup" /><h1>This is Download</h1></>} />
+      <Route path="/" element={<><h1>This is Home</h1></>} />
+      <Route path="/about" element={<><h1>This is About us</h1></>} />
+      <Route path="/contact" element={<><h1>This is contact</h1></>} />
+      <Route path="/download" element={<><h1>This is Download</h1></>} />
       <Route path="/login" element={<Glassform/>} />
       <Route path="/signup" element={<Register/>} />
         
--- frontend/src/components/Glassform.js
+++ frontend/src/components/Glassform.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 
 export default function Glassform() {
+
+  const [email, setEmail]= React.useState('');
+  const [password, setPassword]= React.useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    console.warn("email,password", email, password)
+    let result = await fetch('/auth/login', {
+      method:'post',
+      body:JSON.stringify({email,password}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    result = await result.json();
+    console.warn(result);
+    if (result.user){
+      localStorage.setItem("user", JSON.stringify(result.user));
+      navigate("/chat");
+    }
+  }
+
   return (
     <>
-    <Navbar val="Sign Up"/>
+    {/* <Navbar val="Sign Up"/> */}
       <div className="set">
         <div className="side">
             <div className="wel">
@@ -27,19 +50,20 @@ export default function Glassform() {
             <p className="para1">Log In</p>
             <form action="#">
               <div className="inp">
-              <input type="text" placeholder="Phone or Email"/>
+              <input type="text" placeholder="Phone or Email" 
+              onChange={(e) => setEmail(e.target.value)} value={email}/>
               </div>
               <div className="inp">
-              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="Password" 
+              onChange={(e) => setPassword(e.target.value)} value={password}/>
               </div>
-              <div className="inp but">
+              <div className="inp but" onClick={handleLogin}>
               <input type="submit" value="Log In"/>
               </div>
               <div className="hyper">
               <p>Forgott Password? <a href="
               #">Click me</a></p>
-              <p>Create a Account? <a href="
-              #">Register</a></p>
+              <p>Create a Account? <Link to="/signup">Register</Link></p>
               </div>
 
             </form>
